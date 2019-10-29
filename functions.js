923d7c6b1ea61375123d3069be8d3c4901d461fa
Maria Santamaria <mcsan_16@outlook.com>
2019-10-29 16:37:09 +0000

    adding recursion

--- functions.js
+++ functions.js
@@ -9,3 +9,20 @@ var btn2 = document.getElementById('btn2');
 btn2.addEventListener('click', function(event) {
   console.log('Using function expression');
 });
+
+var n = parseInt(document.getElementById('input_value').value);
+var btn_fact = document.getElementById('btn_factorial');
+
+btn_fact.addEventListener('click', function(event) {
+  var fact = factorial(n);
+  result = document.getElementById('result').innerHTML = fact;
+});
+
+function factorial(n) {
+  if (n <= 1) {
+    return 1;
+  }
+  else {
+    return n * factorial(n - 1);
+  }
+}
