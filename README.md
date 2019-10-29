6b9f983c71851a6deb99644429a7ee94e3a96e3e
Maria Santamaria <mcsan_16@outlook.com>
2019-10-29 14:57:33 +0000

    adding class Figure

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# javascript-tutorial
+# Tutorial Part 2 - JavaScript 6
+
+[https://mariancross.github.io/javascript-tutorial](https://mariancross.github.io/javascript-tutorial)
---
+++ geometric_fig.js
@@ -0,0 +1,26 @@
+class Figure {
+  constructor() {
+    this._area = 0.0;
+    this._perimeter = 0.0;
+  }
+
+  set area(area) {
+    this._area = area;
+  }
+
+  set perimeter(perimeter) {
+    this._perimeter = perimeter;
+  }
+
+  get area() {
+    return this._area;
+  }
+
+  get perimeter() {
+    return this.perimeter;
+  }
+}
+
+var figure = new Figure();
+figure.area = 15;
+console.log(figure.area);
